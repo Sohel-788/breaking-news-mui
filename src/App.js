fa8a9abad0afbb40ff28f92fdb82ea65720fbbda
Sohel-788 <mahmud3724@gmail.com>
2020-09-07 20:25:49 -0500

    under developing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import News from './Components/News/News';
 
 function App() {
+  const [articles, setArticles]= useState([]);
+  useEffect(()=>{
+    const url='https://newsapi.org/v2/top-headlines?country=us&apiKey=e892ab7de87f4bc9a1d2e35c9c17e50e';
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>setArticles(data.articles))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Breaking News designed by M-ui</h1>
+       {
+         articles.map(article=>  <News article={article}></News>)
+       }
+      
       </header>
     </div>
   );
